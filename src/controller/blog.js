cd8d86e5ec921cf75a4356c2db94ef7b91825d97
mengda1027 <mengda1027@qq.com>
2020-10-24 16:07:20 +0800

    complete bolg curd

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,6 +1,7 @@
 const { exec } = require("../db/index")
 
 exports.blogCtl = {
+  // 获取列表
   getList: async (author, keyword) => {
     let sql = "select * from blogs where 1=1"
     if (author) {
@@ -14,5 +15,56 @@ exports.blogCtl = {
   },
   updateBlog: (id, newContent) => {
     return { id, newContent }
+  },
+
+  // 博客详情
+  getDeatail: async (id) => {
+    const sql = `select * from blogs where id='${id}'`
+    const rows = await exec(sql)
+    return rows[0]
+  },
+
+  // 新建
+  newBlog: async (blogData = {}) => {
+    // blogData 包含title content author
+    const title = blogData.title
+    const content = blogData.content
+    const author = blogData.author
+    const createTime = Date.now()
+
+    const sql = `
+        insert into blogs (title, content, createtime, author)
+        values ('${title}', '${content}', ${createTime}, '${author}');
+    `
+    const insertData = await exec(sql)
+    return {
+      id: insertData.insertId,
+    }
+  },
+  // 更新博客
+  updateBlog: async (id, blogData = {}) => {
+    const title = blogData.title
+    const content = blogData.content
+
+    const sql = `
+        update blogs set title='${title}', content='${content}' where id=${id}
+    `
+
+    const updateData = await exec(sql)
+    if (updateData.affectedRows > 0) {
+      return true
+    }
+    return false
+  },
+
+  // 删除
+  delBlog: async (id, author) => {
+    // id 就是要删除博客的id
+    const sql = `delete from blogs where id='${id}' and author='${author}';`
+    const delData = await exec(sql)
+    if (delData.affectedRows > 0) {
+      return true
+    }
+    return false
   },
 }
--- src/router/blog.js
+++ src/router/blog.js
@@ -16,29 +16,41 @@ exports.blogRouter = async (req, res) => {
    * 获取博客详情
    */
   if (req.method === "GET" && req.path === "/api/blog/detail") {
-    return {
-      detial: "detail",
-    }
+    const detail = await blogCtl.getDeatail(req.query.id)
+    return new SuccessModel(detail)
   }
 
   /**
    * 新建一篇博客
    */
   if (req.method === "POST" && req.path === "/api/blog/new") {
-    return {
-      new: req.body,
-    }
+    req.body.author = "wanger"
+    const result = await blogCtl.newBlog(req.body)
+    return new SuccessModel(result)
   }
 
   /**
    * 更新内容
    */
   if (req.method === "POST" && req.path === "/api/blog/update") {
-    const re = blogCtl.updateBlog(id, req.body)
+    const re = await blogCtl.updateBlog(id, req.body)
     if (re) {
-      return new SuccessModel(re)
+      return new SuccessModel()
     } else {
       return new ErrorModel("update fail ... ")
+    }
+  }
+
+  /**
+   * 删除博客
+   */
+  if (req.method === "POST" && req.path === "/api/blog/del") {
+    req.body.author = "wanger"
+    const re = await blogCtl.delBlog(id, req.body.author)
+    if (re) {
+      return new SuccessModel()
+    } else {
+      return new ErrorModel("delete fail ... ")
     }
   }
 }
